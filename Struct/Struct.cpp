fe66fe399d26a548a7f5fbcdf489bbd91372f190
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-03 14:37:16 +0700

    tambah display

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -27,5 +27,13 @@ int main()
 	cout << "Masukkan umur : ";
 	cin >> mhs2.umur;
 
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nnama : " << mhs1.nama;
+	cout << "\nalamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
 
+	cout << "\nNim : " << mhs2.nim;
+	cout << "\nnama : " << mhs2.nama;
+	cout << "\nalamat : " << mhs2.alamat;
+	cout << "\numur : " << mhs2.umur;
 }
