c24b4ccbaa1939d722e045e89d4cd49e76c33be7
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-03 14:38:15 +0700

    tambah comment

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -13,12 +13,12 @@ int main()
 {
 	Mahasiswa mhs1, mhs2;		//Memasukkan data agar bisa dipanggil
 
-	mhs1.nim = "20220140177";
+	mhs1.nim = "20220140177";		//Data 1 langsung terisi
 	mhs1.nama = "Fasya";
 	mhs1.alamat = "Solo";
 	mhs1.umur = 20;
 
-	cout << "Masukkan nim : ";
+	cout << "Masukkan nim : ";		//Data 2 yang dapat mengisi sendiri
 	cin >> mhs2.nim;
 	cout << "Masukkan nama : ";
 	cin >> mhs2.nama;
@@ -27,12 +27,12 @@ int main()
 	cout << "Masukkan umur : ";
 	cin >> mhs2.umur;
 
-	cout << "\nNim : " << mhs1.nim;
+	cout << "\nNim : " << mhs1.nim;			//Display data 1
 	cout << "\nnama : " << mhs1.nama;
 	cout << "\nalamat : " << mhs1.alamat;
 	cout << "\numur : " << mhs1.umur;
 
-	cout << "\nNim : " << mhs2.nim;
+	cout << "\nNim : " << mhs2.nim;			//Display data 2
 	cout << "\nnama : " << mhs2.nama;
 	cout << "\nalamat : " << mhs2.alamat;
 	cout << "\numur : " << mhs2.umur;
