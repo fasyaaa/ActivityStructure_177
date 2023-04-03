8e33924d44a418f33d1f12f58e1bd9a25f0a4822
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-03 14:35:22 +0700

    menambah kriteria mhs 2

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -18,5 +18,14 @@ int main()
 	mhs1.alamat = "Solo";
 	mhs1.umur = 20;
 
+	cout << "Masukkan nim : ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur : ";
+	cin >> mhs2.umur;
+
 
 }
