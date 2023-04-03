0208c374f298675be9cfe857835d1c07386c935d
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-04-03 14:49:58 +0700

    tambah detail alamat

--- Struct2/Struct2.cpp
+++ Struct2/Struct2.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+struct AlamatDetail {				//digunakan untuk melakukan pendetailan terhadap sebuah data
+	string desa;
+	string kota;
+
+};
+
 struct Mahasiswa {				//digunakan untuk pemanggilan tipe data
 	string nim;
 	string nama;
-	string alamat;
+	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+}
